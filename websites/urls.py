88ed0d9d6b49592737f0a20ffe5edff0ea2309e5
Jasmine Chan <chan.jasmine4@gmail.com>
2018-11-16 00:20:27 +0800

    about us link on homepage

--- websites/urls.py
+++ websites/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
 	path('customize/#',views.customize, name='customize'),
 	path('register/', views.signup, name='signup'),
 	path('register/#', views.register, name='register'),
-	
+	path('aboutus/', views.aboutus, name='aboutus')
 ]
